a9396d62f1239a1cc88a10561bf312387b25f847
aulneau <aulneau@users.noreply.github.com>
2021-11-29 08:55:32 -0600

    fix: expand options for handleContractCall

--- packages/react/src/hooks/transaction-signing/use-contract-call.ts
+++ packages/react/src/hooks/transaction-signing/use-contract-call.ts
@@ -1,35 +1,65 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import hash from 'stable-hash';
 
 import { useLoading } from '../use-loading';
 import { useTransactionPopup } from './use-transaction-popup';
 
 import type { MakeContractCallOptions } from './types';
+import { SetOptional } from '../../common/types';
 
-export function useContractCall(options: MakeContractCallOptions) {
-  const key = useMemo(() => hash([options]), [options]);
+export type UseContractCallParams = SetOptional<
+  MakeContractCallOptions,
+  'functionName' | 'functionArgs' | 'postConditionMode' | 'postConditions'
+>;
+
+export interface HandleContractCallParams {
+  functionName?: MakeContractCallOptions['functionName'];
+  functionArgs?: MakeContractCallOptions['functionArgs'];
+  postConditionMode?: MakeContractCallOptions['postConditionMode'];
+  postConditions?: MakeContractCallOptions['postConditions'];
+  onFinish?: MakeContractCallOptions['onFinish'];
+  onCancel?: MakeContractCallOptions['onCancel'];
+}
+
+export function useContractCall(options: UseContractCallParams) {
+  const _key = useMemo(() => hash([options]), [options]);
+
+  const [key, setKey] = useState(_key);
   const [isLoading, setIsLoading] = useLoading(key);
   const popup = useTransactionPopup();
 
   const handleContractCall = useCallback(
-    async (partialOptions?: {
-      onFinish?: MakeContractCallOptions['onFinish'];
-      onCancel?: MakeContractCallOptions['onCancel'];
-    }) => {
+    async (params?: HandleContractCallParams) => {
+      if (!options.functionName && !params?.functionName)
+        throw TypeError('[micro-stacks/react] handleContractCall: functionName is required');
+
+      if (!options.functionArgs && !params?.functionArgs)
+        throw TypeError('[micro-stacks/react] handleContractCall: functionArgs is required');
       setIsLoading(true);
-      await popup.handleContractCall({
+
+      const finalOptions: MakeContractCallOptions = {
         ...options,
+        functionArgs: (params?.functionArgs || options.functionArgs)!,
+        functionName: (params?.functionName || options.functionName)!,
+        postConditionMode: (params?.postConditionMode || options.postConditionMode)!,
+        postConditions: (params?.postConditions || options.postConditions)!,
         onFinish(payload) {
-          partialOptions?.onFinish?.(payload);
+          params?.onFinish?.(payload);
           options?.onFinish?.(payload);
           setIsLoading(false);
         },
         onCancel(payload) {
-          partialOptions?.onCancel?.(payload);
+          params?.onCancel?.(payload);
           options?.onCancel?.(payload);
           setIsLoading(false);
         },
-      });
+      };
+
+      const updatedKey = hash(finalOptions);
+
+      setKey(updatedKey);
+
+      await popup.handleContractCall(finalOptions);
     },
     [options]
   );
