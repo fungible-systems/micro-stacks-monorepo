b41288ae6f4c12d4fa1d061cf8560702eb48b256
aulneau <aulneau@users.noreply.github.com>
2021-12-16 10:37:57 -0600

    fix: types

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -7,8 +7,8 @@ export * from './builders/make-get-server-side-props';
 
 export * from './wrap-with-micro-stacks';
 export { getWithGetInitialProps } from './common/get-with-get-initial-props';
-export { WithGetInitialProps } from './common/get-with-get-initial-props';
-export { GetQueriesWithStxInfo } from './common/get-with-get-initial-props';
-export { WithStacksInfo } from './common/get-with-get-initial-props';
-export { GetInitialProps } from './common/get-with-get-initial-props';
 export { getAtomBuilders } from './common/get-atom-builders';
+export type { WithGetInitialProps } from './common/get-with-get-initial-props';
+export type { GetQueriesWithStxInfo } from './common/get-with-get-initial-props';
+export type { WithStacksInfo } from './common/get-with-get-initial-props';
+export type { GetInitialProps } from './common/get-with-get-initial-props';
