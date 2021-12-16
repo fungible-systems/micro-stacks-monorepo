0e1d7ea2a4aa1a04ed96e6d48ba070e6ad6aca75
aulneau <aulneau@users.noreply.github.com>
2021-12-16 10:29:41 -0600

    fix: allow use of getInitialProps with wrapWithMicroStacks

---
+++ .changeset/smart-dots-help.md
@@ -0,0 +1,5 @@
+---
+'@micro-stacks/nextjs': patch
+---
+
+This update improves the `wrapWithMicrostacks` helper to allow use of `getInitialProps` in place of `getServerSideProps`.
--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -6,3 +6,9 @@ export * from './builders/build-micro-stacks-atoms';
 export * from './builders/make-get-server-side-props';
 
 export * from './wrap-with-micro-stacks';
+export { getWithGetInitialProps } from './common/get-with-get-initial-props';
+export { WithGetInitialProps } from './common/get-with-get-initial-props';
+export { GetQueriesWithStxInfo } from './common/get-with-get-initial-props';
+export { WithStacksInfo } from './common/get-with-get-initial-props';
+export { GetInitialProps } from './common/get-with-get-initial-props';
+export { getAtomBuilders } from './common/get-atom-builders';
