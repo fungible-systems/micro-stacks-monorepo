d21be8bde68f2a9491bdba74d39b28c88b63f839
M. Bagher Abiat <zorofight94@gmail.com>
2022-07-01 16:51:20 +0430

    feat: store

--- packages/svelte/src/store.ts
+++ packages/svelte/src/store.ts
@@ -1,9 +1,46 @@
-import { getClient } from '@micro-stacks/client';
-import {writable} from 'svelte/store'
+import { ClientConfig, defaultStorage, getClient as _getClient } from '@micro-stacks/client';
+import { get, writable } from 'svelte/store';
+import { onAuthentication, onPersistState, onSignOut } from './events';
 
+export const microStacksClientStore = writable<ReturnType<typeof _getClient> | null>();
 
-export const MicroStacksClientStore = writable<ReturnType<typeof getClient> | null>()
+export const mountClient = ({
+  client: client_,
+  dehydratedState,
+  appIconUrl,
+  appName,
+  network,
+  storage = defaultStorage,
+  onPersistState: _onPersistState,
+  onAuthentication: _onAuthentication,
+  onSignOut: _onSignOut,
+}: {
+  client?: ReturnType<typeof _getClient>;
+} & ClientConfig) => {
+  const client =
+    client_ ??
+    _getClient({
+      appName,
+      appIconUrl,
+      dehydratedState,
+      network,
+      storage,
+      onPersistState: _onPersistState,
+      onAuthentication: _onAuthentication,
+      onSignOut: _onSignOut,
+    });
+  microStacksClientStore.set(client);
+  if (!client_) {
+    onAuthentication(_onAuthentication);
+    onSignOut(_onSignOut);
+    onPersistState(_onPersistState);
+  }
+};
 
-
-
-export {}
+export const getClient = () => {
+  const client = get(microStacksClientStore);
+  if (!client) {
+    throw new Error('No MicroStacksClient set, mount the client in your app with `mountClient`');
+  }
+  return client;
+};
